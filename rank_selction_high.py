2a613e73f10834a9b8608b6357cc35001b6acd62
cholberg <cholberg24@gmail.com>
2022-08-09 11:13:40 +0200

    First commit

--- rank_selction_high.py
+++ rank_selction_high.py
@@ -3,17 +3,25 @@ import scipy as sp
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+import multiprocessing
+from tqdm import tqdm
 from statsmodels.tsa.vector_ar import vecm
+
+sns.set_style("whitegrid")
+d = 40
+k = 20
+n = d - k
+T = 200
 
 
 def sample_one(P, sig, T, burn=100):
     q = sig.shape[0]
     y = np.zeros((T + burn + 2, q))
-    err = np.random.multivariate_normal(np.zeros(q), sig, T + burn + 2)
-    y[0] = 0 + err[0, :]
-    y[1] = np.matmul(P, y[0]) + err[1, :]
+    err = np.random.multivariate_normal(np.zeros(q), sig, T + burn + 1)
+    y[0] = 0
+    y[1] = np.matmul(P, y[0]) + err[0]
     for i in range(2, T + burn + 2):
-        y[i] = y[i - 1] + np.matmul(P, y[i - 1]) + err[i]
+        y[i] = y[i - 1] + np.matmul(P, y[i - 1]) + err[i - 1]
     return y[burn:, :]
 
 
@@ -28,6 +36,29 @@ def comm_mat(m, n):
     return M
 
 
+def get_psi(i, g):
+    if i == 0:
+        return np.vstack([np.eye(d), np.zeros((k, d))])
+    psi_i = np.linalg.matrix_power(np.eye(k) + g, i - 1)
+    psi_i = np.hstack([psi_i, np.zeros((k, n))])
+    return np.vstack([g, np.zeros((n, k)), np.eye(k)]).dot(psi_i)
+
+
+def get_gam(i, g, s, s_til):
+    if i >= 1:
+        tmp = []
+        for j in range(50):
+            tmp.append(get_psi(i + j, g).dot(s).dot(get_psi(j, g).T))
+        return np.sum(tmp, axis=0)
+    elif i <= -1:
+        tmp = []
+        for j in range(50):
+            tmp.append(get_psi(j, g).dot(s).dot(get_psi(j - i, g).T))
+        return np.sum(tmp, axis=0)
+    else:
+        return s_til.copy()
+
+
 def get_lseq(a, k):
     lmax = (0.01 + 0.8) * k / 2
     b = 2 * lmax / k - a
@@ -35,7 +66,6 @@ def get_lseq(a, k):
 
 
 def create_params(k, a=0.01):
-    d = 12
     n = d - k
     # Check that a > 0
     assert a > 0, "a > 0 expected, got a={}".format(a)
@@ -86,54 +116,84 @@ def create_params(k, a=0.01):
     return Pi, s, b
 
 
-def est_p(y, r, t):
+def est_eig(y):
     y_del = (y - np.roll(y, 1, axis=0))[1:, :]
     y = y[:-1, :]
-    s_yy = np.einsum("ni,nj->ij", y, y) / t
-    s_dy = np.einsum("ni,nj->ij", y_del, y) / t
+    T = y_del.shape[0]
+    s_yy = np.einsum("ni,nj->ij", y, y) / T
+    s_dy = np.einsum("ni,nj->ij", y_del, y) / T
     s_yd = s_dy.T
-    s_dd = np.einsum("ni,nj->ij", y_del, y_del) / t
+    s_dd = np.einsum("ni,nj->ij", y_del, y_del) / T
     lhs = s_yd.dot(np.linalg.inv(s_dd)).dot(s_dy)
     rhs = s_yy
     lam, g = sp.linalg.eigh(lhs, rhs)
     idx = lam.argsort()[::-1]
-    lam, g = lam[idx], g[:, idx]
-    return s_dy.dot(g[:, :r]).dot(g[:, :r].T)
+    return lam[idx], g[:, idx], s_dy
 
 
-def cov(x):
-    return np.einsum("ij,ik->jk", x, x) / len(x)
+def boot_sample(res, r, P, B=999):
+    T = res.shape[0] + 1
+    out = np.zeros(B)
+    for b in range(B):
+        x_s = np.zeros((T, d))
+        idx = np.random.choice(range(T - 1), T - 1)
+        res_s = res[idx, :]
+        for t in range(1, T):
+            x_s[t, :] = P.dot(x_s[t - 1, :]) + res_s[t - 1, :]
+        lam_s, _, _ = est_eig(x_s)
+        out[b] = -T * np.sum(np.log(1 - lam_s[r:]))
+    return out
+
+
+def rank_test_boot(y, sig=0.05, B=999):
+    T = y.shape[0]
+    d = y.shape[1]
+    lam, g, s_dy = est_eig(y)
+    y_del = (y - np.roll(y, 1, axis=0))[1:, :]
+    y_ = y[:-1, :]
+    for r in range(d):
+        q_r = -T * np.sum(np.log(1 - lam[r:]))
+        Pi_r = s_dy.dot(g[:, :r]).dot(g[:, :r].T)
+        res_r = (y_del.T - Pi_r.dot(y_.T)).T
+        res_r += -np.mean(res_r)
+        P_r = np.eye(d) + Pi_r
+        q_s = boot_sample(res_r, r, P_r, B)
+        p_r = np.mean(q_s > q_r)
+        # print(p_r)
+        if p_r > sig:
+            return r
+    return d
+
+
+def boot_sim(a):
+    Pi, s, b = create_params(k, a)
+    y = sample_one(Pi, s, T)
+    r = rank_test_boot(y, B=299)
+    return {"rank": r, "bias": b[max(k - r, 0)], "a": a}
 
 
 def main():
-    sns.set_theme()
-
-    k = 8
-    a_seq = [0.001, 0.002, 0.003, 0.004, 0.005]
-
-    # Fixed T
-    T = 500
-    nsim = 500
+    nsim = 200
     res = []
-    for a in a_seq:
-        tmp = pd.DataFrame(np.zeros((nsim, 3)))
-        tmp.columns = ["rank", "bias", "a"]
-        Pi, s, b = create_params(k, a)
-        for i in range(nsim):
-            y = sample_one(Pi, s, T)
-            r = vecm.select_coint_rank(y, -1, 0, method="trace").rank
-            tmp.iloc[i, :] = [r, b[max(k - r, 0)], a]
-        res.append(tmp)
+    a_seq = [0.01, 0.03, 0.1, 0.3]
 
-    res = pd.concat(res, axis=0)
+    with multiprocessing.Pool(processes=8) as pool:
+        progress_bar = tqdm(total=len(a_seq) * nsim)
+        # res = pool.map(boot_sim, a_seq * nsim)
+        print("mapping...")
+        results = tqdm(pool.imap(boot_sim, a_seq * nsim), total=len(a_seq) * nsim)
+        print("running...")
+        res = list(results)
+        print("done")
+
+    res = pd.DataFrame(res)
     res["a"] = pd.Categorical(res["a"])
-    res.head()
 
     df_rank = res.groupby(["rank", "a"]).size().reset_index()
     df_rank.rename(columns={0: "freq"}, inplace=True)
     df_rank["freq"] /= nsim
 
-    rr_min, rr_max = 2, 12
+    rr_min, rr_max = 2, 20
     df_bias = []
     for a in a_seq:
         Pi, s, b = create_params(k, a)
@@ -146,10 +206,14 @@ def main():
     df_bias = pd.DataFrame(df_bias)
     df_bias["a"] = pd.Categorical(df_bias["a"])
 
-    plt.figure(figsize=(10, 5))
     sns.set_style("whitegrid")
 
-    palette = ["#008fd5", "#fc4f30", "#e5ae38", "#6d904f", "#8b8b8b"]  # , "#810f7c"]
+    palette = [
+        "#008fd5",
+        "#fc4f30",
+        "#e5ae38",
+        "#6d904f",
+    ]  # , "#8b8b8b"]  # , "#810f7c"]
     plt.figure(figsize=(10, 5))
     fig = sns.lineplot(
         x="rank",
@@ -157,7 +221,7 @@ def main():
         hue="a",
         linewidth=1,
         data=df_rank,
-        palette=sns.color_palette(palette, 5),
+        palette=sns.color_palette(palette, 4),
     )
     sns.scatterplot(
         x="rank",
@@ -165,76 +229,28 @@ def main():
         hue="a",
         legend=False,
         data=df_rank,
-        palette=sns.color_palette(palette, 5),
+        palette=sns.color_palette(palette, 4),
     )
-    plt.axvline(8, 0, 1, alpha=1, color="black", ls="--", linewidth=".7")
+    plt.axvline(k, 0, 1, alpha=1, color="black", ls="--", linewidth=".7")
     fig.set_ylabel("Proportion")
     fig.set_xlabel("Estimated Rank")
     plt.legend(title="$\lambda_{min}$")
-    print("DONE")
-
-    """
-    p = sns.lineplot(x="rank", y="bias", hue="del_lam", linewidth=2, data=df_bias)
-    p.set_ylabel("b")
-    p.set_xlabel("Rank")
-    p.get_legend().remove()
-    plt.savefig("../plots/fixed_T_bias")
+    plt.savefig("../plots/fixed_T_rank_high.pdf")
     plt.clf()
     print("DONE")
-    """
 
-    # Varying T
-    res = []
-    a_seq = [0.001, 0.002, 0.003, 0.004, 0.005]
-    ts = [100 * i for i in range(3, 22)]
-    tmax = np.max(ts)
-    nsim = 500
-    for a in a_seq:
-        Pi, s, b = create_params(k, a)
-        for i in range(nsim):
-            y = sample_one(Pi, s, tmax)
-            for t in ts:
-                y_ = y[:t, :]
-                r = vecm.select_coint_rank(y_, -1, 0, method="trace").rank
-                Pi_hat = est_p(y_, r)
-                b_hat = np.linalg.norm((Pi - Pi_hat).T.ravel()) ** 2
-                res.append(
-                    {
-                        "Asymptotic bias": b[max(k - r, 0)],
-                        "Empirical bias": b_hat,
-                        "Estimator": Pi_hat.T.ravel(),
-                        "Rank": r,
-                        "a": str(a),
-                        "T": t,
-                    }
-                )
-
-    res = pd.DataFrame(res)
-    res.head()
-
-    df_plot = res.groupby(["a", "T"])["Estimator"].apply(
-        lambda x: np.trace(cov(np.stack(x) - np.mean(x)))
-    )
-    df_plot = df_plot.reset_index()
-    df_plot.rename(columns={"Estimator": "variance"}, inplace=True)
-    df_plot["bias"] = res.groupby(["a", "T"])["Asymptotic bias"].mean().values
-    df_plot["MSE"] = res.groupby(["a", "T"])["Empirical bias"].mean().values
-    df_plot["rank"] = res.groupby(["a", "T"])["Rank"].mean().values
-    df_plot.head()
-
-    plt.figure(figsize=(10, 5))
     p = sns.lineplot(
-        x="T",
+        x="rank",
         y="bias",
         hue="a",
         linewidth=1,
-        data=df_plot,
-        palette=sns.color_palette(palette, 5),
+        data=df_bias,
+        palette=sns.color_palette(palette, 4),
     )
-    p.set_ylabel("Average bias")
-    p.set_xlabel("T")
-    p.legend(title="$\lambda_{min}$")
-    plt.savefig("../plots/range_T_rank")
+    p.set_ylabel("Asymptotic Bias")
+    p.set_xlabel("Rank")
+    plt.legend(title="$\lambda_{min}$")
+    plt.savefig("../plots/fixed_T_bias_high.pdf")
     plt.clf()
     print("DONE")
 
